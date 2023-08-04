c7ed5f19e3b7902d4a3a41a318869f95ae9014cb
Benjamin A <benjamin.alvarado1510@gmail.com>
2023-08-04 08:17:25 -0500

    Examen diseño web

--- src/App.js
+++ src/App.js
@@ -6,33 +6,27 @@ import C01componente from './component/C01componente';
 import C04variable from './component/C04variable';
 import C05operador from './component/C05operador';
 import C06matriz from './component/C06matriz';
+import C07matrizOperaciones from './component/C07matrizOperaciones';
 
 function App() {
   return (
     <div>
-     <h1>Componente 1</h1>
-     <C01componente/>
+     <h1> COMPONENTE 7 </h1>
+      <C07matrizOperaciones />
       <br></br>
-     <h1>Componente 2</h1>
-     <C02contador/>
-     <br></br>
+    
+      <h1> COMPONENTE 5 </h1>
+      <C05operador />
+      <br></br>
 
-     <h1>Componente 3</h1>
-     <C03dobleestado/>
-     <br></br>
-     <br></br>
-     <h1>Componente 4</h1>
-     <C04variable xVariable="hoy dia es viernes..."/>
-     <br></br>
+      <h1> COMPONENTE 3 </h1>
+      <C03dobleestado />
+      <br></br>
 
-     <h1>Componente 5</h1>
-     <C05operador/>
-     <br></br>
-
-     <h1>Componente 6</h1>
-     <C06matriz/>
-     <br></br>
-
+      <h1> COMPONENTE 1 </h1>
+      <C01componente />
+      <br></br>
+     
     </div>
   );
 }
